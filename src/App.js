d071a71603fd31490c6411d61f91f1e81505713f
arunbhalli <tomarbhalli@gmail.com>
2021-11-26 18:29:10 +0100

    adds semantic ui for style

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 
 import './App.css';
-
+import Navbar from './componenents /Navbar'
 function App() {
   return (
     <div className="App">
       <header className="App-header">
     Hello world
    <p> My name is arun singh</p>
+   <div>
+     <Navbar/>
+   </div>
       </header>
     </div>
   );
