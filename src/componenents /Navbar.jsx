833287949fb80dde8a7fe29f0eafe8b2354b2388
arunbhalli <tomarbhalli@email.com>
2021-12-06 17:50:33 +0100

    adds material ui and icons in navbar

--- src/componenents /Navbar.jsx
+++ src/componenents /Navbar.jsx
@@ -1,11 +1,21 @@
-import React from 'react'
+import React from 'react';
+import { Language, NotificationsNone, Settings } from '@mui/icons-material';
 
 const Navbar = () => {
   return (
     <div>
       <h1>Navbar designs</h1>
+      <div>
+        <Language />
+      </div>
+      <div>
+        <NotificationsNone />
+      </div>
+      <div>
+        <Settings />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
